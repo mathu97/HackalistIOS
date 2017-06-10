83c23189223b85dae8ddb18e6b74c52ca5f8bc1b
Mathusan Selvarajah <mathusans52@gmail.com>
2017-06-10 00:45:54 -0400

    Getting current date function added

--- HackalistApp/Hackathon.swift
+++ HackalistApp/Hackathon.swift
@@ -11,37 +11,26 @@ import Alamofire
 
 class Hackathon{
     private var _title,
-                _webURL,
-                _location,
-                _host,
-                _startDate,
-                _endDate,
-                _fbURL,
-                _twitterURL,
-                _googlePlusURL,
-                _travel,
-                _highSchoolers,
-                _prize,
-                _cost,
-                _notes: String!
-
-    private var _year,
-                _length,
-                _size: Int!
+    _webURL,
+    _location,
+    _host,
+    _startDate,
+    _endDate,
+    _fbURL,
+    _twitterURL,
+    _googlePlusURL,
+    _travel,
+    _highSchoolers,
+    _prize,
+    _cost,
+    _notes: String!
     
-    init(API_URL: String, hack_Title: String){
-        let hkURL = URL(string: API_URL)!
+    private var _year,
+    _length,
+    _size: Int!
+    
+    init(json : [String: Any]){
         
-        Alamofire.request(hkURL).responseJSON{ response in
-            switch response.result {
-            case .success:
-                print("Hello")
-                let result = response.result
-                print(result)
-            case .failure(let error):
-                print(error)
-            }
-        }
     }
     
 }
--- HackalistApp/MainVC.swift
+++ HackalistApp/MainVC.swift
@@ -10,13 +10,42 @@ import UIKit
 import Alamofire
 
 class MainVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let hk = Hackathon(API_URL: "https://Hackalist.github.io/api/1.0/2017/01.json", hack_Title: "test")
+        let hkData = getData(API_URL: "https://Hackalist.github.io/api/1.0/2017/01.json")
     }
-
-
-
+    
+    func getData(API_URL : String){
+        //Get current date and format it to look like the response startDate
+        let date = Date()
+        var currentDateComp = DateComponents()
+        let calendar = Calendar.current
+        
+        currentDateComp.month = calendar.component(.month, from: date)
+        currentDateComp.day = calendar.component(.day, from: date)
+        let myFormatter = DateFormatter()
+        myFormatter.dateFormat = "MMMM d"
+        let curr_date_data = calendar.date(from: currentDateComp)!
+        let curr_date = myFormatter.string(from: curr_date_data)
+        
+        
+        let year = calendar.component(.year, from: date)
+        
+        
+        let hkURL = URL(string: API_URL)!
+        
+        Alamofire.request(hkURL).responseJSON{ response in
+            switch response.result {
+            case .success:
+                if let json = response.result.value {
+                    print("JSON: \(json)")
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
 }
 
