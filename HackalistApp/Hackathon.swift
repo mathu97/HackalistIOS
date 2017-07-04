d6a34baebbe1702565a92634dd83b1d9b3d6f9c4
Mathusan Selvarajah <mathusans52@gmail.com>
2017-07-03 20:50:20 -0400

    Adding getters to Hackathon class

--- HackalistApp/Hackathon.swift
+++ HackalistApp/Hackathon.swift
@@ -28,7 +28,118 @@ class Hackathon{
     
     private var _travel, _highSchoolers, _prize : Bool
     
+    var title : String{
+        if _title == nil{
+            _title = ""
+        }
+        return _title
+    }
     
+    var webURL : String{
+        if _webURL == nil{
+            _webURL = ""
+        }
+        return _webURL
+    }
+    
+    var location : String{
+        if _location == nil{
+            _location = ""
+        }
+        return _location
+    }
+    
+    var host : String{
+        if _host == nil{
+            _host = ""
+        }
+        return _host
+    }
+    
+    var startDate : String{
+        if _startDate == nil{
+            _startDate = ""
+        }
+        return _startDate
+    }
+
+    var endDate : String{
+        if _endDate == nil{
+            _endDate = ""
+        }
+        return _endDate
+    }
+    
+    var fbURL : String{
+        if _fbURL == nil{
+            _fbURL = ""
+        }
+        return _fbURL
+    }
+
+    var twitterURL : String{
+        if _twitterURL == nil{
+            _twitterURL = ""
+        }
+        return _twitterURL
+    }
+    
+    var googlePlusURL : String{
+        if _googlePlusURL == nil{
+            _googlePlusURL = ""
+        }
+        return _googlePlusURL
+    }
+    
+    var cost : String{
+        if _cost == nil{
+            _cost = ""
+        }
+        return _cost
+    }
+    
+    var notes : String{
+        if _notes == nil{
+            _notes = ""
+        }
+        return _notes
+    }
+    
+    var year : Int {
+        if _year == nil{
+            _year = 0
+        }
+        
+        return _year
+    }
+
+    var length : Int {
+        if _length == nil{
+            _length = 0
+        }
+        
+        return _length
+    }
+    
+    var size : Int {
+        if _size == nil{
+            _size = 0
+        }
+        
+        return _size
+    }
+    
+    var travel : Bool {
+        return _travel
+    }
+
+    var highSchoolers : Bool{
+        return _highSchoolers
+    }
+    
+    var prize : Bool{
+        return _prize
+    }
     
     init(json : [String: String]){
         self._title = json["title"]
