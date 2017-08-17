9933697da635195cd638ce0c551aab5f628815a1
mathu97 <mathusans52@gmail.com>
2017-08-17 13:42:07 -0400

    Adding segue from hackathon cell & back button

---
+++ HackalistApp/HackathonDetailVC.swift
@@ -0,0 +1,33 @@
+//
+//  HackathonDetailVC.swift
+//  HackalistApp
+//
+//  Created by CLQA on 2017-08-17.
+//  Copyright © 2017 Mathusan. All rights reserved.
+//
+
+import UIKit
+
+class HackathonDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func BackBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
